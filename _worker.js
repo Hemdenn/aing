3db318f83b9dc4bd8c62c54fdf9bf2604f99a8c0
Hemdenn <hamdan123wsb@gmail.com>
2025-06-25 19:02:26 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,10 +5,10 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "hemdenparker.web.id"; // Ganti dengan domain utama kalian
 const serviceName = "aing"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = ""; // Ganti dengan email yang kalian gunakan
-const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
-const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const apiKey = "11ab44a712ff726ce3e1f8f73eab755207d61"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiEmail = "mlbbstck2@gmail.com"; // Ganti dengan email yang kalian gunakan
+const accountID = "1089af10ec0b42060d08ed05b0858fb5"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const zoneID = "ff5c32809b551a8964af3f931b1a39c7"; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
 let proxyIP = "";
 let cachedProxyList = [];
